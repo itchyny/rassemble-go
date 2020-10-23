c36d1fec6dae0edbb3374c2e97a53852d5afea36
itchyny <itchyny@hatena.ne.jp>
2020-10-23 20:15:36 +0900

    initial implementation of Join, just concatenating by |

--- rassemble.go
+++ rassemble.go
@@ -1,1 +1,31 @@
 package rassemble
+
+import "regexp/syntax"
+
+// Join patterns and build a regexp pattern.
+func Join(patterns []string) (string, error) {
+	ra := &rassemble{rs: make([]*syntax.Regexp, 0, len(patterns))}
+	for _, pattern := range patterns {
+		if err := ra.add(pattern); err != nil {
+			return "", err
+		}
+	}
+	return ra.String(), nil
+}
+
+type rassemble struct {
+	rs []*syntax.Regexp
+}
+
+func (ra *rassemble) add(pattern string) error {
+	r, err := syntax.Parse(pattern, syntax.POSIX)
+	if err != nil {
+		return err
+	}
+	ra.rs = append(ra.rs, r)
+	return nil
+}
+
+func (ra *rassemble) String() string {
+	return (&syntax.Regexp{Op: syntax.OpAlternate, Sub: ra.rs}).String()
+}
--- rassemble_test.go
+++ rassemble_test.go
@@ -1,1 +1,43 @@
 package rassemble
+
+import "testing"
+
+func TestJoin(t *testing.T) {
+	testCases := []struct {
+		name     string
+		patterns []string
+		expected string
+	}{
+		{
+			name:     "empty",
+			patterns: []string{},
+			expected: "",
+		},
+		{
+			name:     "single literal",
+			patterns: []string{"abc"},
+			expected: "abc",
+		},
+		{
+			name:     "multiple literals",
+			patterns: []string{"abc", "def", "ghi"},
+			expected: "abc|def|ghi",
+		},
+		{
+			name:     "regexps",
+			patterns: []string{"ab*c", "abc+", "bc+"},
+			expected: "ab*c|abc+|bc+",
+		},
+	}
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			got, err := Join(tc.patterns)
+			if err != nil {
+				t.Fatalf("got an error: %s", err)
+			}
+			if got != tc.expected {
+				t.Errorf("expected: %s, got: %s", tc.expected, got)
+			}
+		})
+	}
+}
