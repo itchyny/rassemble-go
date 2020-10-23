b394dfb9b74fb2cbd37755329affae9a0c9ada33
itchyny <itchyny@hatena.ne.jp>
2020-10-23 20:19:06 +0900

    implement a cli tool rassemble

--- cmd/rassemble/main.go
+++ cmd/rassemble/main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+
+	"github.com/itchyny/rassemble-go"
+)
+
+const cmdName = "rassemble"
 
 func main() {
-	fmt.Print("rassemble")
+	pat, err := rassemble.Join(os.Args[1:])
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "%s: %s\n", cmdName, err)
+		os.Exit(1)
+	}
+	fmt.Printf("%s\n", pat)
 }
