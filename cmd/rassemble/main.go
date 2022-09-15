85b4233f801c51baa51717849e4f2b5723c07d15
itchyny <itchyny@cybozu.co.jp>
2022-09-15 21:09:42 +0900

    implement command line tool flags -help and -version

--- cmd/rassemble/main.go
+++ cmd/rassemble/main.go
@@ -2,26 +2,72 @@ package main
 
 import (
 	"bufio"
+	"flag"
 	"fmt"
 	"os"
+	"runtime"
 
 	"github.com/itchyny/rassemble-go"
 )
 
-const cmdName = "rassemble"
+const name = "rassemble"
+
+const version = "0.0.0"
+
+var revision = "HEAD"
 
 func main() {
-	xs := os.Args[1:]
-	if len(xs) == 0 {
+	os.Exit(run(os.Args[1:]))
+}
+
+const (
+	exitCodeOK = iota
+	exitCodeErr
+)
+
+func run(args []string) int {
+	fs := flag.NewFlagSet(name, flag.ContinueOnError)
+	fs.SetOutput(os.Stderr)
+	fs.Usage = func() {
+		fs.SetOutput(os.Stdout)
+		fmt.Printf(`%[1]s - assemble regular expressions
+
+Version: %s (rev: %s/%s)
+
+Synopsis:
+  %% %[1]s re1 re2 ...
+
+Options:
+`, name, version, revision, runtime.Version())
+		fs.PrintDefaults()
+	}
+	var showVersion bool
+	fs.BoolVar(&showVersion, "version", false, "print version")
+	if err := fs.Parse(args); err != nil {
+		if err == flag.ErrHelp {
+			return exitCodeOK
+		}
+		return exitCodeErr
+	}
+	if showVersion {
+		fmt.Printf("%s %s (rev: %s/%s)\n", name, version, revision, runtime.Version())
+		return exitCodeOK
+	}
+	if len(args) == 0 {
 		s := bufio.NewScanner(os.Stdin)
 		for s.Scan() {
-			xs = append(xs, s.Text())
+			args = append(args, s.Text())
+		}
+		if err := s.Err(); err != nil {
+			fmt.Fprintf(os.Stderr, "%s: %s\n", name, err)
+			return exitCodeErr
 		}
 	}
-	pat, err := rassemble.Join(xs)
+	pattern, err := rassemble.Join(args)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "%s: %s\n", cmdName, err)
-		os.Exit(1)
+		fmt.Fprintf(os.Stderr, "%s: %s\n", name, err)
+		return exitCodeErr
 	}
-	fmt.Println(pat)
+	fmt.Println(pattern)
+	return exitCodeOK
 }
